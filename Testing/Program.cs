42b2bb5dfedf4c0c101525f127a0bec822e882c1
Metody Aleksov <teddy57603@gmail.com>
2021-10-22 18:14:44 +0300

    Add project files.

---
+++ Testing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
