2efa863ae4bd1b7e49e42980362446e57dffe778
Metody Aleksov <teddy57603@gmail.com>
2021-10-23 12:04:04 +0300

    something

--- MiniORM/ChangeTracker.cs
+++ MiniORM/ChangeTracker.cs
@@ -1,4 +1,31 @@
-﻿namespace MiniORM
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniORM
 {
 	// TODO: Create your ChangeTracker class here.
+    public class ChangeTracker<T>
+        where T : class, new()
+    {
+        private readonly List<T> allEntities;
+        private readonly List<T> added;
+        private readonly List<T> removed;
+
+        public ChangeTracker(IEnumerable<T> entities)
+        {
+            added = new List<T>();
+            removed = new List<T>();
+
+            allEntities = CloneEntities(entities);
+        }
+
+        private List<T> CloneEntities(IEnumerable<T> entities)
+        {
+            var clonedEntities = new List<T>();
+
+            var propertiesToClone = typeof(T)
+                .GetProperties()
+                .Where(pi => DbContext.AllowedSqlTypes)
+        }
+    }
 }
